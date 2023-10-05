cd2c8b4bdc10ad29830ffa83b31608ee47bcbb74
subramaneshwar_8 <subramaneshwar.redddy@gmail.com>
2023-10-05 11:52:01 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const axios = require('axios');
+const cors = require('cors');
+
+const app = express();
+const port = process.env.PORT || 3001;
+
+app.use(cors());
+
+app.get('/api/data', async (req, res) => {
+  try {
+    const response = await axios.get('http://coding-assignment.bombayrunning.com/data.json');
+    res.json(response.data);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
